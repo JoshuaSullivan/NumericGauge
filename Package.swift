1c6921e8434f7257ec0f6cd85a2032bd5ccb8026
Joshua Sullivan <jsullivan@microsoft.com>
2024-07-04 10:19:18 -0500

    Added dependency on TransientLabel.

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
 // swift-tools-version: 5.10
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
@@ -9,14 +8,14 @@ let package = Package(
         .iOS(.v15)
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "NumericGauge",
             targets: ["NumericGauge"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/JoshuaSullivan/TransientLabel.git", from: "1.0.0"),
+    ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(name: "NumericGauge"),
     ]
 )
